7036aae97ed21ece9a7d9333d8adeb487df156b4
txdk-11 <mily1128@naver.com>
2025-05-30 15:51:14 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function redirectToWiki() {
+    window.location.href = "https://namu.wiki/w/%EA%B0%A4%EB%9F%AD%EC%8B%9C%20%ED%83%AD%20S8";
+}
+
+function redirectToFeatures() {
+    window.location.href = "features.html";
+}
